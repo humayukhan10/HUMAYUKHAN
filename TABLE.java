192ca7862e93872b567d5b0e6e8f542d3cf8e872
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-07-12 21:45:46 +0530

    TABLE.java

---
+++ TABLE.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String[] args) {
+        Scanner op=new Scanner(System.in);
+        int n=op.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+
+            System.out.println(n*i);
+        }
+    }
+}
