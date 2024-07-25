b0d0d92d6cf5320af4b41f072f961141a644c485
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-07-25 21:29:01 +0530

    FIRST.php

---
+++ FIRST.php
@@ -0,0 +1,11 @@
+<?php
+
+// echo "THIS IS VARIABLE PROGRAMME\n";
+$name="HUMAYU KHAN";
+$nameE="HK10";
+
+$income="2000k";
+
+echo "THIS IS GUY ITS NAME IS $name AND ITS INCOME IS $income<br>";
+echo "THIS IS SECONG GUY ITS NAME $nameE";
+?>
