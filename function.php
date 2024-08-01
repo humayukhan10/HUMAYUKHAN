8ae0ab5dc9bf98bc294f9cf96f4e3f1059a8ee73
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-08-01 09:58:54 +0530

    function.php

---
+++ function.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+
+
+<?php
+
+
+
+function cal($a,$b){
+
+$A=$a+$b;
+$S=$a-$b;
+$D=$a/$b;
+$M=$a*$b;
+
+echo "MY SUM IS    ".$A;
+echo "<br>";
+echo "MY SUBSTRACTION  IS    ".$S;
+echo "<br>";
+echo "MY MULTIPICATION IS    ".$M;
+echo "<br>";
+echo "MY DIVISSION  IS    ".$D;
+echo "<br>";
+
+
+}
+
+cal(20,5);
+echo "<br>";
+cal(8,5);
+
+
+
+?>
+
+</body>
+
+</html>
