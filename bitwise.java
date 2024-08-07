f53953bca7173cd8376df7f64018c133ad2a0f45
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-08-07 10:17:58 +0530

    Create bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,20 @@
+public class op {
+ public static void main(String[] args) {
+
+
+
+ int  a = 0b1111 & 0b1000;
+ byte b = 0b0111 | 0b1001;
+ byte c = 0b0101 ^ 0b0100;
+
+ boolean d = ! true;
+ boolean e = ! false;
+ 
+ System.out.println(a);
+ System.out.println(b);
+ System.out.println(c);
+ System.out.println(d);
+ System.out.println(e);
+ 
+ }
+ }
