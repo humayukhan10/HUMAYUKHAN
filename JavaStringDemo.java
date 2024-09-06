6152203159bdd5cbc0adfe3f5deba13b6e3da3cb
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-09-06 21:44:07 +0530

    Create JavaStringDemo.java

---
+++ JavaStringDemo.java
@@ -0,0 +1,13 @@
+public class JavaStringDemo {
+ public static void main(String[] args) {
+ String x = "airplane";
+System.out.println( x.charAt(2) ); // output is 'r'
+String y = "book";
+System.out.println( y.concat(" author")); 
+// output is "book author"
+String z = "01234567";
+System.out.println( z.length() ); // returns "8"
+ }
+}
+
+
