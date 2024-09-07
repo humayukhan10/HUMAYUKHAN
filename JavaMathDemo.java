1f7f986a8d56384c43a5afa0eeac94cae0bbcc50
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-09-07 14:48:31 +0530

    Create JavaMathDemo.java

---
+++ JavaMathDemo.java
@@ -0,0 +1,11 @@
+public class JavaMathDemo {
+ public static void main(String[] args) {
+ 
+System.out.println(Math.abs(-2.3));
+System.out.println(Math.min(2,8));
+System.out.println(Math.max(2,8));
+System.out.println(Math.min(2,8));
+System.out.println(Math.ceil(8.4));
+System.out.println(Math.floor(8.4));
+ }
+}
