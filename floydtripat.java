491df3a476f8d07263b2f34f59fe191a1a62980e
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-09-21 17:16:31 +0530

    Create floydtripat.java

---
+++ floydtripat.java
@@ -0,0 +1,20 @@
+public class floydtripat {
+     public static void main(String[] args) {
+        int i,j,n=5;
+        int number=1;
+
+        for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+
+        System.out.print(number + " ");
+        number++;
+      }
+         System.out.println();
+
+        }
+    
+    
+    
+    }
+    
+}
