89a17794ca57bec2aefbf1be93f3e96c6cba062b
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-09-22 11:20:17 +0530

    Create GUI .java

---
+++ GUI .java
@@ -0,0 +1,18 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+public class GUI {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setTitle("HK APP");
+        frame.setResizable(false);
+        frame.setSize(300,250);
+        frame.setVisible(true);
+
+        ImageIcon image =new ImageIcon("3D-Laptop-Images-Download-Free.jpg");
+        frame.setIconImage(image.getImage());
+
+
+
+
+    }
+}
