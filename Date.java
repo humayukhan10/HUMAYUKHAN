6a1c3d1dd489d26c870ea9bb27cdb5214cfcb7e6
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-09-27 20:05:05 +0530

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+class Date{
+  public static void main(String args[]){
+    LocalDateTime current = LocalDateTime.now();
+    System.out.println("Current Date and Time is: " + current);
+  }
+}
