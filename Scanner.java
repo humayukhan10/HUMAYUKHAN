33faeea06ac8673e68f9f0ac17b51ae526906e38
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-09-28 14:01:48 +0530

    Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Scanner{
+  public static void main(String args[]){
+    Scanner scr = new Scanner(System.in);
+    int a = scr.nextInt();
+    System.out.print(a);
+  }
+}
