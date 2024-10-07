8a1fa582b8772237d0c6c1200ddbd60dc0497bff
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-10-07 20:43:11 +0530

    Create ARRAY.java

---
+++ ARRAY.java
@@ -0,0 +1,13 @@
+public class ARRAY{
+public static void main(String[] args) {
+    int []a=new int[3];
+
+     a[0]=100;
+     a[1]=200;
+     a[2]=300;
+     for(int i=0;i<=2;i++){
+    System.out.print(a[i]);
+     }
+    
+}
+}
