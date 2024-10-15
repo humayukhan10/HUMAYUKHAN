6242a53daef7993b0d60bb4832a84dfcdac2b4ac
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-10-15 20:11:37 +0530

    Create  Inheritances.java

---
+++ Inheritances.java
@@ -0,0 +1,45 @@
+class A{
+    public int a1;
+    public int a2;
+    public void ma1(){
+        System.out.println("ma1");
+    }
+    public void ma2(){
+        System.out.println("ma2");
+    }
+
+}
+class B extends A{
+public int b1;
+public int b2;
+public void b1(){
+    System.out.println("mb1");
+}
+public void b2(){
+    System.out.println("mb2");
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+public class Inheritances{
+public static void main(String[] args) {
+    A a=new A();
+    B b=new B();
+    a.ma1();
+    a.ma2();
+    b.ma1();
+    b.ma2();
+    b.b1();
+    b.b2();
+    
+}    
+}
