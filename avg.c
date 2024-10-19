813c74b820900e6c0d7bd83c4c3b18ebb099d017
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-10-19 16:35:19 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main (void){
+
+    int a[2][2],i,j;
+
+
+printf("ENTER A NUBER:");
+scanf("[%d][%d]",&i,j);
+
+for(i=0;i<2;i++){
+    for(j=0;j<2;j++){
+
+
+        printf("%d",a[i][j]);
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+}
