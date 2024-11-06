25d924379d148a1bac6ca12a1b03f77cb9a787f1
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-11-06 12:03:05 +0530

    Create Reminder.java

---
+++ Reminder.java
@@ -0,0 +1,9 @@
+class Reminder {
+  public static void main(String[] args){
+
+    int dividend = 27, divisor = 6;
+    int remainder = dividend % divisor;
+    System.out.println("Remainder = " + remainder);
+      
+  }
+}
