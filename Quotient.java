a195907fabb79bc3614767789c9096bfd6fec94f
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-11-08 16:08:05 +0530

    Create Quotient.java

---
+++ Quotient.java
@@ -0,0 +1,9 @@
+class Quotient{
+  public static void main(String[] args){
+
+    int dividend = 27, divisor = 6;
+    int quotient = dividend / divisor;
+    System.out.println("Quotient = " + quotient);
+      
+  }
+}
