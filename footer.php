4076cbcc10fafdd9480e956c90618c6334508ea6
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2024-12-08 10:47:01 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+		</td>
+	</tr>
+	<tr bgcolor=#a00>
+		<td colspan=2 align=right>
+			<font color=#fff><b>Design and Developed by: CAIT-Anand</b></font>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
