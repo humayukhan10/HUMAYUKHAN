486767f4b946efeabf9c8d772512f94bca65f367
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-01-12 20:25:59 +0530

    Create var.js

---
+++ var.js
@@ -0,0 +1,15 @@
+var a=18;
+
+{
+   if(a<=18){
+      console.log("you are eligble for voting")
+      }
+
+}
+if(a<=18){
+console.log("you are eligble for voting")
+}
+else {
+   console.log("you are not eliigble for voting")
+
+}
