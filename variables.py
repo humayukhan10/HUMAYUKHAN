51df27961dc815f12ad30a2ac6d0c428f884a6ca
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-06-10 09:24:39 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+age=23
+price=23.5
+name="hk"
+boll=True
+a=None
+
+print("My age is",age)
+print("my price is",price)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(boll))
+print(type(a))
