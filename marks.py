4b38d56c2537f7fc94ffdaf537ff698290f68bd5
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-06-14 08:24:51 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,9 @@
+marks=int (input(" Enter Your Marks"))
+if(marks >= 91):
+    print("You Got A Grade")
+elif (marks >= 80 and marks <90):
+        print("You Got B Grade") 
+elif (marks >=70 and marks<80):
+     print("You Got C Grade") 
+else:
+      print("You Got D GRADE")
