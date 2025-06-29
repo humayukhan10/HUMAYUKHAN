33e02c2adf14e1b325a7f9fa0b57dec003911b0c
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-06-29 18:12:18 +0530

    Create for_searchh.py

---
+++ for_searchh.py
@@ -0,0 +1,9 @@
+x=81
+i=0
+tup=(1,4,9,25,36,49,64,81,100)
+for val in tup :
+        if(val== x):
+           print("your element at",i)
+           i +=1
+        else:
+                print("not found")
