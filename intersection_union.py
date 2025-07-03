bcd9a0bde39548f063f318b98f41787b58e94560
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-07-03 18:57:51 +0530

    Create intersection_union.py

---
+++ intersection_union.py
@@ -0,0 +1,7 @@
+#INTERSECTION =>COMBINETWO SET AND RETURNS THE COMMON VALUES IN BOTH SET
+
+set3={"applle",6,7,8,9}
+set4={"applle",6,1,2,11,9}
+
+print(set4.intersection(set3))
+print(set3.union(set4))
