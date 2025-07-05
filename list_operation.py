97ed64893ee5ade1f4f515bc0eb2be88e9cdaf05
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-07-05 10:30:31 +0530

    Create list_operation.py

---
+++ list_operation.py
@@ -0,0 +1,26 @@
+list=[2,1,3]
+
+list.append(4)#add elment in list in the last
+print(list)
+
+list.sort()  #sort list in ascending order
+print(list)
+
+list.sort(reverse=True) #sort list in descending order
+print(list)
+
+list.reverse() #print the list in reverse form
+print(list)
+
+#insert element in index
+list.insert(1,"karan")
+print(list)
+
+#remove the elment
+list.remove(1)
+print(list)
+
+#remove elment at index
+
+list.pop(2)
+print(list)
