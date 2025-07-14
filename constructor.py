81530c7383db72221a52bb8dac2d0af197cc4591
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-07-14 13:23:45 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,10 @@
+#constructor
+class Human():
+    def __init__(self,name):
+       self.name=name
+       print("Eating.....")
+        
+        
+        
+s1=Human("sohil bhai") 
+print(s1.name)  
