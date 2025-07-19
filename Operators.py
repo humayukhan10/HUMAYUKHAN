e885d35599d407807305b774e700589a69428c12
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-07-19 10:04:01 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,33 @@
+#arithmetic operators
+
+a=5
+b=6
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a % b)
+
+#relational operators
+
+print(a==b)
+print(a !=b)
+
+#Assigement Operators
+
+a=a+10
+a *=10
+print(a)
+
+#logical not,or,and
+
+print(not  False )
+print(not (a<b))
+
+val1=True
+val2=False
+
+print("And Operators" ,val1 and val2)
+print("OR Operators" ,val1 or val2)
