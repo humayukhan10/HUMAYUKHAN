aaf4a71b166f1f8be154983ee2c7ff4bbec0db12
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-07-31 12:24:10 +0530

    Create Array_index.c

---
+++ Array_index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (void){
+
+   int arr[5]={10,20,30,40,50};
+
+   printf("first elements [%d]\n",arr[0]);
+   printf("second elements [%d]\n",arr[1]);
+   printf("third elements [%d]\n",arr[2]);
+   printf("fourth elements [%d]\n",arr[3]);
+   printf("fifth elements [%d]\n",arr[4]);
+
+   return 0;
+
+}
+
