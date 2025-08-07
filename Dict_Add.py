5585a5a823decd12010f59a6cef9a70e31672031
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-08-07 12:58:34 +0530

    Create Dict_Add.py

---
+++ Dict_Add.py
@@ -0,0 +1,16 @@
+#dictonary
+
+dic={
+     "name":"ayan",
+     "sem":5,
+     "RollNo:":7,
+     "Regno":"3060823008"
+     
+     }
+
+print(dic.items())
+
+dic.update({"name":"adking","hk":"noob"})   # here hk is not in dis but the authmoticaly add
+print(dic)
+
+dic.get("name")   # rturn the value
