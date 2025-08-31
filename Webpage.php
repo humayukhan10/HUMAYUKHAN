7f547bbdd3f935dcc2831bf7abcab497b26f63c5
PATHAN HUMAYU KHAN IMRANKKHAN <152798311+humayukhan10@users.noreply.github.com>
2025-08-31 17:45:40 +0530

    Create Webpage.php

---
+++ Webpage.php
@@ -0,0 +1,37 @@
+
+
+<table color=pink align=center>
+
+	<tr bgcolor=gray><th><font size=20 color=>Welcome MY Site</font></th></tr>
+	<tr  bgcolor=#f19c79 height=30>
+			<td colspan=2>
+			<A href=>Home</A> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			<A href=>Student</A> &nbsp;&nbsp;&nbsp;
+			<A href=>Contact Us</A> &nbsp;&nbsp;&nbsp;
+			<A href=>Change Password</A> &nbsp;&nbsp;&nbsp;
+			<A href=>Logout</A> &nbsp;&nbsp;&nbsp;
+			
+
+			</td>
+		</tr>
+		
+			<tr>
+				<td colspan=3>
+				</td>
+			</tr>
+<tr>
+	<td>
+	
+	
+	</td>
+</tr>
+			<tr bgcolor=596f62>
+			<td colspan=2 align=right>
+			<font color=#fff><b>Design and Developed by: Ayan Diwan</b></font>
+			</td>
+			</tr>
+			
+			
+			</table>	
+		</body>
+</html>
